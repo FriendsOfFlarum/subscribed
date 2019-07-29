72ce7d237a7d9c35d867c53bea7cfadd193e385e
Christian Lopez <Ralkage@live.com>
2019-07-29 15:34:44 -0400

    DRY it up ;)

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -9,14 +9,16 @@ app.initializers.add('fof-subscribed', (app) => {
     app.notificationComponents.userCreated = UserCreatedNotification;
 
     extend(NotificationGrid.prototype, 'notificationTypes', (items) => {
-        if (app.session.user && app.forum.attribute('subscribeDiscussionCreated')) {
+        if (!app.session.user) return;
+
+        if (app.forum.attribute('subscribeDiscussionCreated')) {
             items.add('discussionCreated', {
                 name: 'discussionCreated',
                 icon: 'fas fa-pencil-alt',
                 label: app.translator.trans('fof-subscribed.forum.settings.notify_discussion_created_label')
             }, 5);
         }
-        if (app.session.user && app.forum.attribute('subscribeUserCreated')) {
+        if (app.forum.attribute('subscribeUserCreated')) {
             items.add('userCreated', {
                 name: 'userCreated',
                 icon: 'fas fa-user-plus',
