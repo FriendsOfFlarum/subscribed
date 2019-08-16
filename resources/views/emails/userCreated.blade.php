210164b1692af707236c56139870bcd40edebf42
Evgenij Spitsyn <spev@gurtam.com>
2019-08-16 16:53:39 +0300

    Fix email subject for "user created" mail

--- resources/views/emails/userCreated.blade.php
+++ resources/views/emails/userCreated.blade.php
@@ -3,4 +3,4 @@ Hey {{ $user->username }}!
 {{ $blueprint->user->username }} just joined {{ app()->url() }}.
 
 To view this new user's profile, please click on following link:
-{{ app()->url() }}/u/{{ $blueprint->user->id }}
+{{ app()->url() }}/u/{{ $blueprint->user->username }}
