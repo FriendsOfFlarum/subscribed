83c6f4c59f2a6234fb70368bf0a4d93a6e0a9d6d
Ian Morland <ian@morland.me>
2020-12-10 11:48:13 +0000

    Fix  issue

--- resources/views/emails/userCreated.blade.php
+++ resources/views/emails/userCreated.blade.php
@@ -1,6 +1,6 @@
 {!! $translator->trans('fof-subscribed.email.body.newUser', [
     '{recipient_display_name}' => $user->display_name,
     '{actor_display_name}' => $blueprint->user->display_name,
-    '{forum_url}' => $url->to('forum'),
-    '{user_url}' => $this->url->to('forum')->route('user', ['username' => $blueprint->user->username]),
+    '{forum_url}' => $url->to('forum')->base(),
+    '{user_url}' => $url->to('forum')->route('user', ['username' => $blueprint->user->username]),
 ]) !!}
