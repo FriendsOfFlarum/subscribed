1284086ae31ae8d443b3d9e78c5d94b225b65aa9
IanM <ian@morland.me>
2026-03-13 19:32:48 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('fof-subscribed', () => {
-  app.extensionData
+  app.registry
     .for('fof-subscribed')
     .registerPermission(
       {
