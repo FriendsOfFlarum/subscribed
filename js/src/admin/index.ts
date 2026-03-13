ef1730e3f0a509b4a0a24c555dc036f083801273
IanM <ian@morland.me>
2026-03-13 20:13:33 +0000

    chore(2.0): migrate admin permission registrations to extend.ts

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,46 +1,7 @@
 import app from 'flarum/admin/app';
 
+export { default as extend } from './extend';
+
 app.initializers.add('fof-subscribed', () => {
-  app.registry
-    .for('fof-subscribed')
-    .registerPermission(
-      {
-        icon: 'fas fa-bell',
-        label: app.translator.trans('fof-subscribed.admin.permission.subscribe_to_discussion_created'),
-        permission: 'subscribeDiscussionCreated',
-      },
-      'start'
-    )
-    .registerPermission(
-      {
-        icon: 'fas fa-bell',
-        label: app.translator.trans('fof-subscribed.admin.permission.subscribe_to_post_created'),
-        permission: 'subscribePostCreated',
-      },
-      'start'
-    )
-    .registerPermission(
-      {
-        icon: 'fas fa-bell',
-        label: app.translator.trans('fof-subscribed.admin.permission.subscribe_to_user_created'),
-        permission: 'subscribeUserCreated',
-      },
-      'start'
-    )
-    .registerPermission(
-      {
-        icon: 'fas fa-hammer',
-        label: app.translator.trans('fof-subscribed.admin.permission.subscribe_to_post_unapproved'),
-        permission: 'subscribePostUnapproved',
-      },
-      'moderate'
-    )
-    .registerPermission(
-      {
-        icon: 'fas fa-flag',
-        label: app.translator.trans('fof-subscribed.admin.permission.subscribe_to_post_flagged'),
-        permission: 'subscribePostFlagged',
-      },
-      'moderate'
-    );
+  // Permissions are registered via extend.ts
 });
