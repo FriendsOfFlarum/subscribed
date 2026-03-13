a4dd7851ce04ba847773da379e114c73bae7b157
IanM <ian@morland.me>
2026-03-13 20:14:27 +0000

    chore: remove admin initializer

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,1 @@
-import app from 'flarum/admin/app';
-
 export { default as extend } from './extend';
-
-app.initializers.add('fof-subscribed', () => {
-  // Permissions are registered via extend.ts
-});
